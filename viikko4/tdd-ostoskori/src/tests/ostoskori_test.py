4b8b32c5f10e248cfd4bf2faa4acc30334140a87
Joni Joensuu <joni.joens@gmail.com>
2022-11-22 20:13:53 +0200

    Kahden saman tuotteen lisäämisen jälkeen ostoskorissa on 2 tavaraa

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -33,3 +33,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.juusto)
 
         self.assertEqual(self.kori.hinta(), 9)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_kaksi_tavaraa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+        
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
